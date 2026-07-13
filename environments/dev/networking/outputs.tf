8482eded2f34034cfdd0a2dcfbb7ff0b02a68c98
pagotiramarao <pagotiramarao26@gmail.com>
2026-07-14 01:40:40 +0530

    fix: correct nat and igw outputs

--- environments/dev/networking/outputs.tf
+++ environments/dev/networking/outputs.tf
@@ -13,3 +13,11 @@ output "private_app_subnet_ids" {
 output "private_db_subnet_ids" {
   value = module.vpc.private_db_subnet_ids
 }
+
+output "nat_gateway_id" {
+  value = module.vpc.nat_gateway_id
+}
+
+output "internet_gateway_id" {
+  value = module.vpc.internet_gateway_id
+}
