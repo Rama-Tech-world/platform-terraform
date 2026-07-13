38455f9623fe74809b6b79a2f11d9cd289eb94ca
pagotiramarao <pagotiramarao26@gmail.com>
2026-07-14 01:03:16 +0530

    feat: add public private app and db subnets

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -5,3 +5,15 @@ output "vpc_id" {
 output "vpc_cidr" {
   value = aws_vpc.this.cidr_block
 }
+
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_app_subnet_ids" {
+  value = aws_subnet.private_app[*].id
+}
+
+output "private_db_subnet_ids" {
+  value = aws_subnet.private_db[*].id
+}
