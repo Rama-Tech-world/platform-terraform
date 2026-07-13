ac1c87c92716f68fc9d7b0d9484079799f40c6f0
pagotiramarao <pagotiramarao26@gmail.com>
2026-07-14 00:58:07 +0530

    retail-platform-dev-vpc creation

--- environments/dev/networking/terraform.tfvars
+++ environments/dev/networking/terraform.tfvars
@@ -6,26 +6,22 @@ vpc_cidr = "10.0.0.0/16"
 
 availability_zones = [
   "eu-north-1a",
-  "eu-north-1b",
-  "eu-north-1c"
+  "eu-north-1b"
 ]
 
 public_subnet_cidrs = [
   "10.0.0.0/20",
-  "10.0.16.0/20",
-  "10.0.32.0/20"
+  "10.0.16.0/20"
 ]
 
 private_app_subnet_cidrs = [
   "10.0.48.0/20",
-  "10.0.64.0/20",
-  "10.0.80.0/20"
+  "10.0.64.0/20"
 ]
 
 private_db_subnet_cidrs = [
   "10.0.96.0/20",
-  "10.0.112.0/20",
-  "10.0.128.0/20"
+  "10.0.112.0/20"
 ]
 
 enable_flow_logs   = true
---
+++ modules/vpc/variables.tf
@@ -0,0 +1,37 @@
+variable "project_name" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "availability_zones" {
+  type = list(string)
+}
+
+variable "public_subnet_cidrs" {
+  type = list(string)
+}
+
+variable "private_app_subnet_cidrs" {
+  type = list(string)
+}
+
+variable "private_db_subnet_cidrs" {
+  type = list(string)
+}
+
+variable "enable_flow_logs" {
+  type    = bool
+  default = true
+}
+
+variable "single_nat_gateway" {
+  type    = bool
+  default = true
+}
