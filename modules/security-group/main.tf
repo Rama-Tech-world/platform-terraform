bfa6182a4142e3e6dcb3e6444292b384e3d903e6
pagotiramarao <pagotiramarao26@gmail.com>
2026-07-14 08:45:19 +0530

    modifying security module stack

--- modules/security-group/main.tf
+++ modules/security-group/main.tf
@@ -43,14 +43,102 @@ resource "aws_security_group" "alb" {
   )
 }
 
-output "eks_cluster_sg_id" {
-  value = aws_security_group.eks_cluster.id
+resource "aws_vpc_security_group_ingress_rule" "cluster_from_nodes" {
+
+  security_group_id = aws_security_group.eks_cluster.id
+
+  referenced_security_group_id = aws_security_group.eks_node.id
+
+  from_port = 443
+  to_port   = 443
+
+  ip_protocol = "tcp"
+
+  description = "Allow worker nodes to communicate with EKS API"
 }
 
-output "eks_node_sg_id" {
-  value = aws_security_group.eks_node.id
+resource "aws_vpc_security_group_ingress_rule" "node_to_node" {
+
+  security_group_id = aws_security_group.eks_node.id
+
+  referenced_security_group_id = aws_security_group.eks_node.id
+
+  ip_protocol = "-1"
+
+  description = "Node to node communication"
 }
 
-output "alb_sg_id" {
-  value = aws_security_group.alb.id
+resource "aws_vpc_security_group_ingress_rule" "cluster_to_kubelet" {
+
+  security_group_id = aws_security_group.eks_node.id
+
+  referenced_security_group_id = aws_security_group.eks_cluster.id
+
+  from_port = 10250
+  to_port   = 10250
+
+  ip_protocol = "tcp"
+
+  description = "Cluster to kubelet communication"
+}
+
+resource "aws_vpc_security_group_egress_rule" "node_all_egress" {
+
+  security_group_id = aws_security_group.eks_node.id
+
+  cidr_ipv4 = "0.0.0.0/0"
+
+  ip_protocol = "-1"
+
+  description = "Allow all outbound traffic"
+}
+
+resource "aws_vpc_security_group_egress_rule" "cluster_all_egress" {
+
+  security_group_id = aws_security_group.eks_cluster.id
+
+  cidr_ipv4 = "0.0.0.0/0"
+
+  ip_protocol = "-1"
+
+  description = "Allow outbound traffic"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "alb_http" {
+
+  security_group_id = aws_security_group.alb.id
+
+  cidr_ipv4 = "0.0.0.0/0"
+
+  from_port = 80
+  to_port   = 80
+
+  ip_protocol = "tcp"
+
+  description = "HTTP from internet"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "alb_https" {
+
+  security_group_id = aws_security_group.alb.id
+
+  cidr_ipv4 = "0.0.0.0/0"
+
+  from_port = 443
+  to_port   = 443
+
+  ip_protocol = "tcp"
+
+  description = "HTTPS from internet"
+}
+
+resource "aws_vpc_security_group_egress_rule" "alb_all_egress" {
+
+  security_group_id = aws_security_group.alb.id
+
+  cidr_ipv4 = "0.0.0.0/0"
+
+  ip_protocol = "-1"
+
+  description = "ALB outbound traffic"
 }
---
+++ modules/security-group/outputs.tf
@@ -0,0 +1,11 @@
+output "eks_cluster_sg_id" {
+  value = aws_security_group.eks_cluster.id
+}
+
+output "eks_node_sg_id" {
+  value = aws_security_group.eks_node.id
+}
+
+output "alb_sg_id" {
+  value = aws_security_group.alb.id
+}
