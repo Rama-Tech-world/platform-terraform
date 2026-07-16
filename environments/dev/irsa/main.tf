b0294696e858bb97bf64f5d6cf38c6d31b0e6daa
pagotiramarao <pagotiramarao26@gmail.com>
2026-07-16 23:20:29 +0530

    external DNS policy attachment

--- environments/dev/irsa/main.tf
+++ environments/dev/irsa/main.tf
@@ -35,3 +35,22 @@ module "alb_controller_irsa" {
     "arn:aws:iam::219834006508:policy/retail-platform-dev-alb-controller-policy"
   ]
 }
+
+module "external_dns_irsa" {
+
+  source = "../../../modules/irsa"
+
+  role_name = "${var.project_name}-${var.environment}-external-dns-role"
+
+  oidc_provider_arn = data.terraform_remote_state.eks.outputs.oidc_provider_arn
+
+  oidc_provider_url = data.terraform_remote_state.eks.outputs.oidc_issuer_url
+
+  namespace = "external-dns"
+
+  service_account_name = "external-dns"
+
+  policy_arns = [
+    "arn:aws:iam::219834006508:policy/retail-platform-dev-external-dns-policy"
+  ]
+}
--- environments/dev/irsa/outputs.tf
+++ environments/dev/irsa/outputs.tf
@@ -5,3 +5,7 @@ output "external_secrets_role_arn" {
 output "alb_controller_role_arn" {
   value = module.alb_controller_irsa.role_arn
 }
+
+output "external_dns_role_arn" {
+  value = module.external_dns_irsa.role_arn
+}
